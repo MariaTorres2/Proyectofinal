d2176c9e65995b834323b234b66810144eb0dfe7
ccortesm49 <ccortesm2000@gmail.com>
2024-11-17 01:33:03 -0500

    cambios en el token del ligin y ruta de conexion de front

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,6 +1,6 @@
 import Usuario from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import { createToken} from "../libs/jwt.js";
+import { createToken } from "../libs/jwt.js";
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
@@ -14,29 +14,27 @@ export const login = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "Usuario no encontrado" });
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ error: "Contraseña incorrecta" });
     }
-    const token = await createToken ({ id: user._id,  username: user.username,}
-    );
-    console.log("Token generado:", token);
-   
 
-res.cookie("token", token, { 
-  httpOnly: true,
-  secure: true, 
-  sameSite: "none", 
-});
-console.log("Encabezados de respuesta:", res.getHeaders());
-console.log("salto");
-console.log("Cookies configuradas:", res.getHeaders()["set-cookie"]);
+    const token = await createToken({ id: user._id, username: user.username });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
     res.status(200).json({
       token,
       user: {
         id: user._id,
         nombre_persona: user.nombre_persona,
         email: user.email,
+        rol: user.rol
       },
     });
   } catch (error) {
@@ -45,12 +43,12 @@ console.log("Cookies configuradas:", res.getHeaders()["set-cookie"]);
   }
 };
 
-
-export const logout = async (req, res) => {
-  res.cookie("Token", "", {
-    // httpOnly: true,
-    // secure: true,
+export const logout = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
     expires: new Date(0),
   });
-  return res.sendStatus(200);
+  return res.status(200).json({ message: "Sesión cerrada correctamente" });
 };
